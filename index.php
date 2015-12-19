312951e4c4857f6997eb5ddacba78d7eaf4f69a1
Wiki59 <wiki59@live.fr>
2015-12-19 11:04:50 +0100

    Page d'inscription en pé ache pay

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <html>
 <head>
-    <title>Test PHP</title>
+    <meta charset="UTF-8">
+    <title>Index</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php
+<div id="entete">
+    <?php
+    $connexion = 2;
+    if ($connexion === 2) { ?>
+        <ul>
+            <li><a href="#">Connexion</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+        </ul>
+    <?php } else { ?>
+        <ul>
+            <li><a href="#">Deconnexion</a></li>
+        </ul>
+    <?php } ?>
+</div>
 
-echo '<p>Bonjour le monde</p>';
-
-?>
+<div id="menu">
+    <p>
+        Bienvenue bonhomme, t'as envie de parier avec tes pote, tu te trouves au bon endroit
+    </p>
+</div>
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Wissam
- * Date: 19/12/2015
- * Time: 10:22
- */
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<form action="inscription.php" method="post" id="inscriptionform">
+    <ul>
+        <li><h1>Inscription :</h1></li><br>
+        <li>Nom : </li><li><input type="text" name="nom"></li><br>
+        <li>Prenom :</li><li><input type="text" name="prenom"></li><br>
+        <li>Age :</li><li><input type="text" name="age"></li><br>
+        <li>e-mail</li><li><input type="email" name="mail"></li><br>
+        <li>Mot de passe :</li><li><input type="password" name="mdp1"></li><br>
+        <li>Vérification du mot de passe :</li><li><input type="password" name="mdp2"></li><br>
+        <li><input type="submit"></li>
+    </ul>
+</form>
+</body>
+</html>
