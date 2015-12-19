30b700b79d1bf091c8a72f33fba5d6741ea9cafd
Wiki59 <wiki59@live.fr>
2015-12-19 07:28:02 +0100

    La base

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Test PHP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+
+echo '<p>Bonjour le monde</p>';
+
+?>
+</body>
+</html>
